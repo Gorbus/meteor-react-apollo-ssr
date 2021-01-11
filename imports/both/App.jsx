58b4333bd29ef19b868046569690a2446a85a51e
Nathan Schwarz <nathan.schwarz97@gmail.com>
2021-01-11 10:25:44 +0100

    udpated App.jsx to include helmet-async

--- imports/both/App.jsx
+++ imports/both/App.jsx
@@ -2,15 +2,19 @@ import React from "react";
 import { ApolloProvider } from "@apollo/client";
 import Routes from "./../ui/Routes";
 import Router from "./Router";
+import { Helmet, HelmetProvider } from "react-helmet-async"
 
-export default function App({ client, location }) {
-  const helmetContext = {};
-
+export default function App({ client, location, context = {}}) {
   return (
     <ApolloProvider client={client}>
-      <Router location={location}>
-        <Routes />
-      </Router>
+      <HelmetProvider context={helmetContext}>
+        <Helmet>
+          <title>Meteor React Apollo SSR</title>
+        </Helmet>
+        <Router location={location}>
+          <Routes />
+        </Router>
+      </HelmetProvider>
     </ApolloProvider>
   );
 }
