d8720d98d4a21d1266c70ab092122663cf1bdb46
Nathan Schwarz <nathan.schwarz97@gmail.com>
2021-05-06 16:31:16 +0200

    patched single Apollo Client instance

--- imports/both/apolloClient.jsx
+++ imports/both/apolloClient.jsx
@@ -2,15 +2,18 @@ import { Meteor } from "meteor/meteor";
 import { ApolloClient, InMemoryCache, createHttpLink } from "@apollo/client";
 import fetch from "cross-fetch";
 
-const cache = Meteor.isClient
-  ? new InMemoryCache().restore(window.__APOLLO_STATE__)
-  : new InMemoryCache();
+function cache() {
+  if (Meteor.isClient) {
+    return new InMemoryCache().restore(window.__APOLLO_STATE__)
+  }
+  return new InMemoryCache()
+}
 
 export default function getApolloClient() {
   return new ApolloClient({
     ssrMode: Meteor.isServer,
     link: createHttpLink({ uri: Meteor.absoluteUrl('/graphql'), fetch }),
-    cache,
+    cache: cache(),
     ssrForceFetchDelay: 100,
   })
 }
