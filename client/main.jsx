50831ef44d4bd274ffbfcb12bf809e378cd02e7f
Ivonig Corfmat <ivonig.corfmat@gmail.com>
2021-05-31 16:44:29 +0200

    updated packages and solved client created multiple times

--- client/main.jsx
+++ client/main.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import App from "../imports/both/App";
 import { onPageLoad } from "meteor/server-render";
-import apolloClient from "../imports/both/apolloClient";
+import getApolloClient from "../imports/both/apolloClient";
 import { hydrate } from "react-dom";
 
+const client = getApolloClient();
+
 onPageLoad((sink) =>
-  hydrate(<App client={apolloClient} />, document.getElementById("app"))
+  hydrate(<App client={client} />, document.getElementById("app"))
 );
--- imports/both/apolloClient.jsx
+++ imports/both/apolloClient.jsx
@@ -2,13 +2,18 @@ import { Meteor } from "meteor/meteor";
 import { ApolloClient, InMemoryCache, createHttpLink } from "@apollo/client";
 import fetch from "cross-fetch";
 
-const cache = Meteor.isClient
-  ? new InMemoryCache().restore(window.__APOLLO_STATE__)
-  : new InMemoryCache();
+function cache() {
+  if (Meteor.isClient) {
+    return new InMemoryCache().restore(window.__APOLLO_STATE__);
+  }
+  return new InMemoryCache();
+}
 
-export default apolloClient = new ApolloClient({
-  ssrMode: Meteor.isServer,
-  link: createHttpLink({ uri: Meteor.absoluteUrl('/graphql'), fetch }),
-  cache,
-  ssrForceFetchDelay: 100,
-})
+export default function getApolloClient() {
+  return new ApolloClient({
+    ssrMode: Meteor.isServer,
+    link: createHttpLink({ uri: Meteor.absoluteUrl("/graphql"), fetch }),
+    cache: cache(),
+    ssrForceFetchDelay: 100,
+  });
+}
